b9a96e7083937df8a9eddb3338b921fd4aff0109
Alberto Sierra <alberto.sierra@murciaeduca.es>
2025-01-21 09:47:22 +0000

    created file exercises/514ab134-1fd6-4f3c-b7a8-e1a59f302221/libraries/d0b032bf-0a96-4ed8-adda-9b3e0f877aa7/peliculasGrande.js

---
+++ exercises/514ab134-1fd6-4f3c-b7a8-e1a59f302221/libraries/d0b032bf-0a96-4ed8-adda-9b3e0f877aa7/peliculasGrande.js
@@ -0,0 +1,450 @@
+db.peliculas.insertMany([{
+  "_id": "664e0656205d7eee44deb16c",
+  "pelicula_id": 730,
+  "titulo": "Ridgemont Submarine",
+  "categoria": "New",
+  "precio": 0.99,
+  "duracion": 46,
+  "clasificacion": "PG-13"
+},
+{
+  "_id": "664e0656205d7eee44deb16d",
+  "pelicula_id": 892,
+  "titulo": "Titanic Boondock",
+  "categoria": "Animation",
+  "precio": 4.99,
+  "duracion": 104,
+  "clasificacion": "R"
+},
+{
+  "_id": "664e0656205d7eee44deb16e",
+  "pelicula_id": 254,
+  "titulo": "Driver Annie",
+  "categoria": "Sports",
+  "precio": 2.99,
+  "duracion": 159,
+  "clasificacion": "PG-13"
+},
+{
+  "_id": "664e0656205d7eee44deb16f",
+  "pelicula_id": 541,
+  "titulo": "Luke Mummy",
+  "categoria": "Animation",
+  "precio": 2.99,
+  "duracion": 74,
+  "clasificacion": "NC-17"
+},
+{
+  "_id": "664e0656205d7eee44deb170",
+  "pelicula_id": 112,
+  "titulo": "Calendar Gunfight",
+  "categoria": "Foreign",
+  "precio": 4.99,
+  "duracion": 120,
+  "clasificacion": "NC-17"
+},
+{
+  "_id": "664e0656205d7eee44deb171",
+  "pelicula_id": 211,
+  "titulo": "Darling Breaking",
+  "categoria": "Games",
+  "precio": 4.99,
+  "duracion": 165,
+  "clasificacion": "PG-13"
+},
+{
+  "_id": "664e0656205d7eee44deb172",
+  "pelicula_id": 841,
+  "titulo": "Star Operation",
+  "categoria": "Sports",
+  "precio": 2.99,
+  "duracion": 181,
+  "clasificacion": "PG"
+},
+{
+  "_id": "664e0656205d7eee44deb173",
+  "pelicula_id": 406,
+  "titulo": "Haunting Pianist",
+  "categoria": "Games",
+  "precio": 0.99,
+  "duracion": 181,
+  "clasificacion": "R"
+},
+{
+  "_id": "664e0656205d7eee44deb174",
+  "pelicula_id": 193,
+  "titulo": "Crossroads Casualties",
+  "categoria": "Animation",
+  "precio": 2.99,
+  "duracion": 153,
+  "clasificacion": "G"
+},
+{
+  "_id": "664e0656205d7eee44deb175",
+  "pelicula_id": 504,
+  "titulo": "Kwai Homeward",
+  "categoria": "Drama",
+  "precio": 0.99,
+  "duracion": 46,
+  "clasificacion": "PG-13"
+},
+{
+  "_id": "664e0656205d7eee44deb176",
+  "pelicula_id": 580,
+  "titulo": "Mine Titans",
+  "categoria": "New",
+  "precio": 4.99,
+  "duracion": 166,
+  "clasificacion": "PG-13"
+},
+{
+  "_id": "664e0656205d7eee44deb177",
+  "pelicula_id": 676,
+  "titulo": "Philadelphia Wife",
+  "categoria": "Sports",
+  "precio": 4.99,
+  "duracion": 137,
+  "clasificacion": "PG-13"
+},
+{
+  "_id": "664e0656205d7eee44deb178",
+  "pelicula_id": 502,
+  "titulo": "Knock Warlock",
+  "categoria": "Comedy",
+  "precio": 2.99,
+  "duracion": 71,
+  "clasificacion": "PG-13"
+},
+{
+  "_id": "664e0656205d7eee44deb179",
+  "pelicula_id": 194,
+  "titulo": "Crow Grease",
+  "categoria": "Action",
+  "precio": 0.99,
+  "duracion": 104,
+  "clasificacion": "PG"
+},
+{
+  "_id": "664e0656205d7eee44deb17a",
+  "pelicula_id": 739,
+  "titulo": "Rocky War",
+  "categoria": "Drama",
+  "precio": 4.99,
+  "duracion": 145,
+  "clasificacion": "PG-13"
+},
+{
+  "_id": "664e0656205d7eee44deb17b",
+  "pelicula_id": 989,
+  "titulo": "Working Microcosmos",
+  "categoria": "Travel",
+  "precio": 4.99,
+  "duracion": 74,
+  "clasificacion": "R"
+},
+{
+  "_id": "664e0656205d7eee44deb17c",
+  "pelicula_id": 992,
+  "titulo": "Wrath Mile",
+  "categoria": "Documentary",
+  "precio": 0.99,
+  "duracion": 176,
+  "clasificacion": "NC-17"
+},
+{
+  "_id": "664e0656205d7eee44deb17d",
+  "pelicula_id": 358,
+  "titulo": "Gilmore Boiled",
+  "categoria": "Classics",
+  "precio": 0.99,
+  "duracion": 163,
+  "clasificacion": "R"
+},
+{
+  "_id": "664e0656205d7eee44deb17e",
+  "pelicula_id": 829,
+  "titulo": "Spinal Rocky",
+  "categoria": "Family",
+  "precio": 2.99,
+  "duracion": 138,
+  "clasificacion": "PG-13"
+},
+{
+  "_id": "664e0656205d7eee44deb17f",
+  "pelicula_id": 816,
+  "titulo": "Snowman Rollercoaster",
+  "categoria": "Animation",
+  "precio": 0.99,
+  "duracion": 62,
+  "clasificacion": "G"
+},
+{
+  "_id": "664e0656205d7eee44deb180",
+  "pelicula_id": 222,
+  "titulo": "Desert Poseidon",
+  "categoria": "Horror",
+  "precio": 4.99,
+  "duracion": 64,
+  "clasificacion": "R"
+},
+{
+  "_id": "664e0656205d7eee44deb181",
+  "pelicula_id": 367,
+  "titulo": "Goldmine Tycoon",
+  "categoria": "Sci-Fi",
+  "precio": 0.99,
+  "duracion": 153,
+  "clasificacion": "R"
+},
+{
+  "_id": "664e0656205d7eee44deb182",
+  "pelicula_id": 650,
+  "titulo": "Pacific Amistad",
+  "categoria": "Documentary",
+  "precio": 0.99,
+  "duracion": 144,
+  "clasificacion": "G"
+},
+{
+  "_id": "664e0656205d7eee44deb183",
+  "pelicula_id": 555,
+  "titulo": "Mallrats United",
+  "categoria": "Comedy",
+  "precio": 0.99,
+  "duracion": 133,
+  "clasificacion": "PG"
+},
+{
+  "_id": "664e0656205d7eee44deb184",
+  "pelicula_id": 390,
+  "titulo": "Guys Falcon",
+  "categoria": "Sci-Fi",
+  "precio": 4.99,
+  "duracion": 84,
+  "clasificacion": "R"
+},
+{
+  "_id": "664e0656205d7eee44deb185",
+  "pelicula_id": 956,
+  "titulo": "Wanda Chamber",
+  "categoria": "Games",
+  "precio": 4.99,
+  "duracion": 107,
+  "clasificacion": "PG-13"
+},
+{
+  "_id": "664e0656205d7eee44deb186",
+  "pelicula_id": 673,
+  "titulo": "Personal Ladybugs",
+  "categoria": "Music",
+  "precio": 0.99,
+  "duracion": 118,
+  "clasificacion": "PG-13"
+},
+{
+  "_id": "664e0656205d7eee44deb187",
+  "pelicula_id": 738,
+  "titulo": "Rocketeer Mother",
+  "categoria": "Foreign",
+  "precio": 0.99,
+  "duracion": 178,
+  "clasificacion": "PG-13"
+},
+{
+  "_id": "664e0656205d7eee44deb188",
+  "pelicula_id": 697,
+  "titulo": "Primary Glass",
+  "categoria": "Action",
+  "precio": 0.99,
+  "duracion": 53,
+  "clasificacion": "G"
+},
+{
+  "_id": "664e0656205d7eee44deb189",
+  "pelicula_id": 187,
+  "titulo": "Cranes Reservoir",
+  "categoria": "Sports",
+  "precio": 2.99,
+  "duracion": 57,
+  "clasificacion": "NC-17"
+},
+{
+  "_id": "664e0656205d7eee44deb18a",
+  "pelicula_id": 305,
+  "titulo": "Fatal Haunted",
+  "categoria": "New",
+  "precio": 2.99,
+  "duracion": 91,
+  "clasificacion": "PG"
+},
+{
+  "_id": "664e0656205d7eee44deb18b",
+  "pelicula_id": 641,
+  "titulo": "Orange Grapes",
+  "categoria": "Foreign",
+  "precio": 0.99,
+  "duracion": 76,
+  "clasificacion": "PG-13"
+},
+{
+  "_id": "664e0656205d7eee44deb18c",
+  "pelicula_id": 515,
+  "titulo": "Legally Secretary",
+  "categoria": "Children",
+  "precio": 4.99,
+  "duracion": 113,
+  "clasificacion": "PG"
+},
+{
+  "_id": "664e0656205d7eee44deb18d",
+  "pelicula_id": 90,
+  "titulo": "Boulevard Mob",
+  "categoria": "New",
+  "precio": 0.99,
+  "duracion": 63,
+  "clasificacion": "R"
+},
+{
+  "_id": "664e0656205d7eee44deb18e",
+  "pelicula_id": 559,
+  "titulo": "Married Go",
+  "categoria": "Sci-Fi",
+  "precio": 2.99,
+  "duracion": 114,
+  "clasificacion": "G"
+},
+{
+  "_id": "664e0656205d7eee44deb18f",
+  "pelicula_id": 352,
+  "titulo": "Gathering Calendar",
+  "categoria": "Games",
+  "precio": 0.99,
+  "duracion": 176,
+  "clasificacion": "PG-13"
+},
+{
+  "_id": "664e0656205d7eee44deb190",
+  "pelicula_id": 212,
+  "titulo": "Darn Forrester",
+  "categoria": "Action",
+  "precio": 4.99,
+  "duracion": 185,
+  "clasificacion": "G"
+},
+{
+  "_id": "664e0656205d7eee44deb191",
+  "pelicula_id": 203,
+  "titulo": "Daisy Menagerie",
+  "categoria": "Sci-Fi",
+  "precio": 4.99,
+  "duracion": 84,
+  "clasificacion": "G"
+},
+{
+  "_id": "664e0656205d7eee44deb192",
+  "pelicula_id": 609,
+  "titulo": "Muscle Bright",
+  "categoria": "Travel",
+  "precio": 2.99,
+  "duracion": 185,
+  "clasificacion": "G"
+},
+{
+  "_id": "664e0656205d7eee44deb193",
+  "pelicula_id": 394,
+  "titulo": "Hamlet Wisdom",
+  "categoria": "Sci-Fi",
+  "precio": 2.99,
+  "duracion": 146,
+  "clasificacion": "R"
+},
+{
+  "_id": "664e0656205d7eee44deb194",
+  "pelicula_id": 149,
+  "titulo": "Christmas Moonshine",
+  "categoria": "Children",
+  "precio": 0.99,
+  "duracion": 150,
+  "clasificacion": "NC-17"
+},
+{
+  "_id": "664e0656205d7eee44deb195",
+  "pelicula_id": 418,
+  "titulo": "Hobbit Alien",
+  "categoria": "Drama",
+  "precio": 0.99,
+  "duracion": 157,
+  "clasificacion": "PG-13"
+},
+{
+  "_id": "664e0656205d7eee44deb196",
+  "pelicula_id": 750,
+  "titulo": "Run Pacific",
+  "categoria": "New",
+  "precio": 0.99,
+  "duracion": 145,
+  "clasificacion": "R"
+},
+{
+  "_id": "664e0656205d7eee44deb197",
+  "pelicula_id": 95,
+  "titulo": "Breakfast Goldfinger",
+  "categoria": "New",
+  "precio": 4.99,
+  "duracion": 123,
+  "clasificacion": "G"
+},
+{
+  "_id": "664e0656205d7eee44deb198",
+  "pelicula_id": 335,
+  "titulo": "Freedom Cleopatra",
+  "categoria": "Comedy",
+  "precio": 0.99,
+  "duracion": 133,
+  "clasificacion": "PG-13"
+},
+{
+  "_id": "664e0656205d7eee44deb199",
+  "pelicula_id": 891,
+  "titulo": "Timberland Sky",
+  "categoria": "Classics",
+  "precio": 0.99,
+  "duracion": 69,
+  "clasificacion": "G"
+},
+{
+  "_id": "664e0656205d7eee44deb19a",
+  "pelicula_id": 645,
+  "titulo": "Others Soup",
+  "categoria": "Travel",
+  "precio": 2.99,
+  "duracion": 118,
+  "clasificacion": "PG"
+},
+{
+  "_id": "664e0656205d7eee44deb19b",
+  "pelicula_id": 615,
+  "titulo": "Nash Chocolat",
+  "categoria": "Animation",
+  "precio": 2.99,
+  "duracion": 180,
+  "clasificacion": "PG-13"
+},
+{
+  "_id": "664e0656205d7eee44deb19c",
+  "pelicula_id": 542,
+  "titulo": "Lust Lock",
+  "categoria": "Action",
+  "precio": 2.99,
+  "duracion": 52,
+  "clasificacion": "G"
+},
+{
+  "_id": "664e0656205d7eee44deb19d",
+  "pelicula_id": 468,
+  "titulo": "Invasion Cyclone",
+  "categoria": "Children",
+  "precio": 2.99,
+  "duracion": 97,
+  "clasificacion": "PG"
+}]);
