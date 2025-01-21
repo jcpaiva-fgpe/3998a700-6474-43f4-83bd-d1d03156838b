f3e2075fb9fa949ba4039b4344d47c66977a8cae
Alberto Sierra <alberto.sierra@murciaeduca.es>
2025-01-21 11:08:42 +0000

    updated file exercises/6367d6a1-06f4-4b12-9753-cac2b2a451d6/solutions/512d0703-0b84-469b-bbb1-f5dc2ad0a7e7/solution.js

--- exercises/6367d6a1-06f4-4b12-9753-cac2b2a451d6/solutions/512d0703-0b84-469b-bbb1-f5dc2ad0a7e7/solution.js
+++ exercises/6367d6a1-06f4-4b12-9753-cac2b2a451d6/solutions/512d0703-0b84-469b-bbb1-f5dc2ad0a7e7/solution.js
@@ -1,1 +1,46 @@
-db.createCollection("empleados")
+db.empleados.insertMany([{
+  "empno": 1,
+  "fname": "charlie",
+  "lname": "rodgers",
+  "role": "manager",
+  "departments": ["sales", "marketing"]
+},
+{
+  "empno": 2,
+  "fname": "sunil",
+  "lname": "chakraborty",
+  "role": "team lead",
+  "departments": ["marketing", "finance"]
+},
+{
+  "empno": 3,
+  "fname": "sally",
+  "lname": "jones",
+  "role": "team lead",
+  "departments": ["hr", "admin"]
+},
+{
+  "empno": 4,
+  "fname": "ben",
+  "lname": "bradley",
+  "role": "manager",
+  "departments": ["legal"]
+},
+{
+  "empno": 5,
+  "fname": "radha",
+  "lname": "desai",
+  "role": "worker"
+},
+{
+  "empno": 6,
+  "fname": "shruti",
+  "lname": "patel",
+  "role": "worker"
+},
+{
+  "empno": 7,
+  "fname": "mahesh",
+  "lname": "iyer",
+  "role": "manager"
+}])
