79066130dfa8b0ed70eae3bb7b209c6b1d55b77b
Alberto Sierra <alberto.sierra@murciaeduca.es>
2025-01-21 10:32:06 +0000

    updated file exercises/2d7f84fe-270f-474b-85b0-9938ba707c49/solutions/a34d0d32-b37f-4d00-87b4-5a3770de1917/solution.js

--- exercises/2d7f84fe-270f-474b-85b0-9938ba707c49/solutions/a34d0d32-b37f-4d00-87b4-5a3770de1917/solution.js
+++ exercises/2d7f84fe-270f-474b-85b0-9938ba707c49/solutions/a34d0d32-b37f-4d00-87b4-5a3770de1917/solution.js
@@ -1,1 +1,7 @@
-db.createCollection("empleados")
+db.empleados.insertOne({
+	    "fname": "John",
+        "lname": "Doe",
+        "salary": 70000,
+        "departments": ["sales", "admin"],
+        "hiredate": "2018-08-29"
+    })
