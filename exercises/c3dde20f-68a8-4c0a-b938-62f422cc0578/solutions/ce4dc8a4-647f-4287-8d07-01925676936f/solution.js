c12d20168040843671b6f1de6bb4029cf8abbd20
Alberto Sierra <alberto.sierra@murciaeduca.es>
2025-01-23 10:33:39 +0000

    created file exercises/c3dde20f-68a8-4c0a-b938-62f422cc0578/solutions/ce4dc8a4-647f-4287-8d07-01925676936f/solution.js

---
+++ exercises/c3dde20f-68a8-4c0a-b938-62f422cc0578/solutions/ce4dc8a4-647f-4287-8d07-01925676936f/solution.js
@@ -0,0 +1,7 @@
+db.empleados.insertOne({
+	    "fname": "John",
+        "lname": "Doe",
+        "salary": 70000,
+        "departments": ["sales", "admin"],
+        "hiredate": "2018-08-29"
+    })
