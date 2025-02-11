12090fc67cd06dc0247cabf1f5810aa8e8bcac28
Alberto Sierra <alberto.sierra@murciaeduca.es>
2025-02-11 21:49:04 +0000

    updated file exercises/03a3ed32-e953-47e9-b77b-a5688ce30993/solutions/4c186d1d-b94b-4573-bc00-19e1a2530e93/solution.js

--- exercises/03a3ed32-e953-47e9-b77b-a5688ce30993/solutions/4c186d1d-b94b-4573-bc00-19e1a2530e93/solution.js
+++ exercises/03a3ed32-e953-47e9-b77b-a5688ce30993/solutions/4c186d1d-b94b-4573-bc00-19e1a2530e93/solution.js
@@ -1,14 +1,9 @@
 db.advanced_books.find(
-    {
-        $or: [
-            {"stats.pages": {$gt: 1000}},
-            {"price": {$gt: 30}}
-        ]
-    },
+    {"available": true},
     {
         "_id": 0,
         "title": 1,
-        "stats.pages": 1,
+        "published.year": 1,
         "price": 1
     }
-).sort({"stats.pages": -1})
+).sort({"published.year": 1, "price": -1})
